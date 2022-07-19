c554433b26e44db0cc5cb27b1c9b9c2ce3459437
Marah Massey <96992138+marahmassey@users.noreply.github.com>
2022-07-19 12:01:10 -0400

    Add files via upload

---
+++ src/main/java/system/ris/SystemInfo.java
@@ -0,0 +1,13 @@
+package system.ris;
+
+public class SystemInfo {
+
+    public static String javaVersion() {
+        return System.getProperty("java.version");
+    }
+
+    public static String javafxVersion() {
+        return System.getProperty("javafx.version");
+    }
+
+}
